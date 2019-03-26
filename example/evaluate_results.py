f194d9899a043436d09f8e3e350a6bfd07703398
Gergely Svigruha <sgergo88@gmail.com>
2019-03-25 23:11:28 -0400

    Remove prints.

--- example/evaluate_results.py
+++ example/evaluate_results.py
@@ -1,7 +1,9 @@
 def evaluate(predicted_y, test_y):
   n = 0
-  s = 0
+  good = 0
   for p_y, t_y in zip(predicted_y, test_y):
-    s = s + (p_y - t_y) * (p_y - t_y)
+    t_y = t_y[0]
+    if p_y == t_y:
+      good = good + 1
     n = n + 1
-  return s / n
+  return {'accuracy': (good / n), 'num_samples': n}
--- example/train.py
+++ example/train.py
@@ -4,6 +4,4 @@ from sklearn import tree
 def train(X, y):
   clf = tree.DecisionTreeClassifier()
   clf = clf.fit(X, y)
-  print(X)
-  print(y)
   return clf
