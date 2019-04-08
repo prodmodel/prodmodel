dbaf4ddb77a3ccfe4c5ea17234739e0ced49de84
Gergely Svigruha <sgergo88@gmail.com>
2019-04-07 21:28:00 -0400

    Fix names.

--- prodmodel/model/py_file.py
+++ prodmodel/model/py_file.py
@@ -9,18 +9,17 @@ class PyFile(Artifact):
   def __init__(self, file_name: str):
     super().__init__(file_name=file_name)
     self.mod = None
-    self.current_hash_id = None
-    self.current_build_time = None
+    self.cashed_build_time = None
 
 
   def init(self, args):
-    if self.current_build_time != args.build_time:
-      self.current_hash_id = self.hash_id()
-      self.current_build_time = args.build_time
+    if self.cashed_build_time != args.build_time:
+      self.cached_hash_id = self.hash_id()
+      self.cashed_build_time = args.build_time
 
 
   def output(self):
-    if self.mod is None or self.current_hash_id != self.hash_id():
+    if self.mod is None or self.cached_hash_id != self.hash_id():
       spec = importlib.util.spec_from_file_location(self.hash_id(), self.file_name)
       mod = importlib.util.module_from_spec(spec)
       spec.loader.exec_module(mod)
