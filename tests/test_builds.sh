8239eea6e446fb82320fca6164df5289e6cbc42d
Gergely Svigruha <sgergo88@gmail.com>
2019-04-13 23:24:41 -0400

    Fix tests.

--- tests/test_builds.sh
+++ tests/test_builds.sh
@@ -5,6 +5,6 @@ BASEDIR=$(dirname "$0")/..
 source $BASEDIR/.env
 cd $BASEDIR
 
-python3.6 prodmodel/main.py test_transform
-python3.6 prodmodel/main.py evaluate
-python3.6 prodmodel/main.py model_in_s3
+python3.6 prodmodel/main.py example:test_transform
+python3.6 prodmodel/main.py example:evaluate
+python3.6 prodmodel/main.py example:model_in_s3
