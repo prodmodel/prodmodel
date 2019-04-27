62ee4daa2871e3e970513eb8b4b48283a8a5b231
Gergely Svigruha <sgergo88@gmail.com>
2019-04-27 17:11:46 -0400

    Add docs and docgen.

--- prodmodel/rules/aws_rules.py
+++ prodmodel/rules/aws_rules.py
@@ -8,4 +8,7 @@ from util import checkargtypes
 
 @checkargtypes
 def copy_to_s3(data: Target, s3_bucket: str, s3_key: str) -> S3DataTarget:
+  '''Copies the result of `data` target to `s3_bucket`/`s3_key`.
+     The S3 credentials come from the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars.'''
+ 
   return S3DataTarget(data, s3_bucket, s3_key)
