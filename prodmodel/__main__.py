4637591d9ca71e88af71b4a377f00cab9eabfb0c
Gergely Svigruha <sgergo88@gmail.com>
2019-05-19 18:50:45 -0400

    Fix.

--- prodmodel/__main__.py
+++ prodmodel/__main__.py
@@ -14,6 +14,13 @@ from prodmodel.util import red_color, green_color
 
 
 def main():
+  parser = argparse.ArgumentParser(description='Build, deploy and test Python data science models.')
+  parser.add_argument('target', help='The target to execute in a <path_to_build_file>:<target> format, or <target> if the command is executed from the directory of the build file.')
+  parser.add_argument('--force_external', action='store_true', help='Force reloading external data sources instead of using the cached data.')
+  parser.add_argument('--cache_data', action='store_true', help='Cache local data files.')
+  parser.add_argument('--target_dir', type=str, default='.target', help='The target directory to build in.')
+  parser.add_argument('--build_time', type=int, default=int(time.time()))
+
   executor.setup()
   if hasattr(prodmodel, '__version__'):
     logging.debug(f'Running Prodmodel version {prodmodel.__version__}.')
@@ -32,11 +39,4 @@ def main():
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Build, deploy and test Python data science models.')
-  parser.add_argument('target', help='The target to execute in a <path_to_build_file>:<target> format, or <target> if the command is executed from the directory of the build file.')
-  parser.add_argument('--force_external', action='store_true', help='Force reloading external data sources instead of using the cached data.')
-  parser.add_argument('--cache_data', action='store_true', help='Cache local data files.')
-  parser.add_argument('--target_dir', type=str, default='.target', help='The target directory to build in.')
-  parser.add_argument('--build_time', type=int, default=int(time.time()))
-
   main()
