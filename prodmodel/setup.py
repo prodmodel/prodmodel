64605d0bf0c672ba0ad5db5e3b897b23498ab25e
Gergely Svigruha <sgergo88@gmail.com>
2019-05-19 16:49:28 -0400

    Bump release.

--- prodmodel/setup.py
+++ prodmodel/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='prodmodel',
-    version='0.1.4',
+    version='0.1.5',
     author='Gergely Svigruha',
     author_email='gergely.svigruha@prodmodel.com',
     description='Build data science pipelines and models',
