2e51ac9c7ed540542c92a5071fdb72dd63ac4364
Gergely Svigruha <sgergo88@gmail.com>
2019-05-28 21:15:51 -0400

    Change dirs.

--- tests/prodmodel/model/target_test_util.py
+++ tests/prodmodel/model/target_test_util.py
@@ -11,7 +11,8 @@ class TargetTestUtil(unittest.TestCase):
 
   @classmethod
   def setUp(self):
-    TargetConfig.target_base_dir = Path(os.getcwd())
+    root = Path(os.getcwd())
+    TargetConfig.target_base_dir = root / '.target'
     args = executor.create_arg_parser().parse_args()
-    self.data_file = DataFile(TargetConfig.target_base_dir / 'tests' / 'prodmodel' / 'data' / 'test.json')
+    self.data_file = DataFile(root / 'tests' / 'prodmodel' / 'data' / 'test.json')
     self.data_file.init(args)
