c9dbae0a1272186df3bcc566a2aaff4ff06aaf96
Gergely Svigruha <sgergo88@gmail.com>
2019-05-28 20:24:55 -0400

    Add unit tests for all rules.

--- tests/prodmodel/rules/test_rules.py
+++ tests/prodmodel/rules/test_rules.py
@@ -29,3 +29,16 @@ class TestRules(unittest.TestCase):
   def test_transform(self):
     target = undertest.transform(file='/home/abc/x.py', fn='tf', streams={'s': self._data_stream()})
     self.assertEqual('TransformDataTarget', target.__class__.__name__)
+
+  def test_create_label_encoder(self):
+    target = undertest.create_label_encoder(data=self._data_stream(), columns=['x'])
+    self.assertEqual('LabelEncoderTarget', target.__class__.__name__)
+
+  def test_encode_labels(self):
+    le = undertest.create_label_encoder(data=self._data_stream(), columns=['x'])
+    target = undertest.encode_labels(data=self._data_stream(), label_encoder=le)
+    self.assertEqual('EncodeLabelDataTarget', target.__class__.__name__)
+
+  def test_test(self):
+    target = undertest.test(test_file='/home/abc/x.py')
+    self.assertEqual('TestTarget', target.__class__.__name__)
