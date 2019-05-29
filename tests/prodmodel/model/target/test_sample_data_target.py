c89f6dcd854930c1be641752afe11ddb6eea847a
Gergely Svigruha <sgergo88@gmail.com>
2019-05-28 21:41:38 -0400

    Addsample target test.

---
+++ tests/prodmodel/model/target/test_sample_data_target.py
@@ -0,0 +1,13 @@
+from tests.prodmodel.model.target_test_util import TargetTestUtil
+from prodmodel.model.target.csv_data_target import CSVDataTarget
+from prodmodel.model.target import sample_data_target as undertest
+
+
+class TestSampleDataTarget(TargetTestUtil):
+
+  def test_sample_data_target(self):
+    csv_target = CSVDataTarget(source=self.csv_data_file, dtypes={'a': int, 'b': str})
+    target = undertest.SampleDataTarget(csv_target, 0.5, 1)
+    items = [item for item in target]
+    self.assertEqual(1, len(items))
+    self.assertEqual([{'a': 1, 'b': 'x'}], items)
