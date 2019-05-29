563e685557389a7d6cbc57a4044c3d6e5ac610e5
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2019-05-28 22:39:45 -0500

    Release works on github too (#73)
    * Change release script.
    
    * Fix.
    
    * Fix script.

--- release.sh
+++ release.sh
@@ -16,7 +16,21 @@ if [ -z "$TWINE_PASSWORD" ]
     echo "No TWINE_PASSWORD specified."
     exit 1
 fi
+if [ -z "$GITHUB_API_USERNAME" ]
+  then
+    echo "No GITHUB_API_USERNAME specified."
+    exit 1
+fi
+if [ -z "$GITHUB_API_TOKEN" ]
+  then
+    echo "No GITHUB_API_TOKEN specified."
+    exit 1
+fi
 
 export PRODMODEL_RELEASE_VERSION=$1
+export GITHUB_POST_DATA="{\"tag_name\": \"v${PRODMODEL_RELEASE_VERSION}\", \"target_commitish\": \"master\", \"name\": \"v${PRODMODEL_RELEASE_VERSION}\", \"draft\": false, \"prerelease\": false}"
+
+curl -u $GITHUB_API_USERNAME:$GITHUB_API_TOKEN -X POST https://api.github.com/repos/prodmodel/prodmodel/releases -d "$GITHUB_POST_DATA"
+
 python3.6 prodmodel/setup.py sdist bdist_wheel
 twine upload dist/prodmodel-"$PRODMODEL_RELEASE_VERSION"*
