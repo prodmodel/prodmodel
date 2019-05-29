f645e4d342e94107f051be04d51c54392517259b
Gergely Svigruha <sgergo88@gmail.com>
2019-05-28 21:45:47 -0400

    Add select data target.

--- tests/prodmodel/model/target/test_sample_data_target.py
+++ tests/prodmodel/model/target/test_sample_data_target.py
@@ -7,7 +7,7 @@ class TestSampleDataTarget(TargetTestUtil):
 
   def test_sample_data_target(self):
     csv_target = CSVDataTarget(source=self.csv_data_file, dtypes={'a': int, 'b': str})
-    target = undertest.SampleDataTarget(csv_target, 0.5, 1)
+    target = undertest.SampleDataTarget(csv_target, ratio=0.5, seed=1)
     items = [item for item in target]
     self.assertEqual(1, len(items))
     self.assertEqual([{'a': 1, 'b': 'x'}], items)
---
+++ tests/prodmodel/model/target/test_select_data_target.py
@@ -0,0 +1,21 @@
+from tests.prodmodel.model.target_test_util import TargetTestUtil
+from prodmodel.model.target.csv_data_target import CSVDataTarget
+from prodmodel.model.target import select_data_target as undertest
+
+
+class TestSelectDataTarget(TargetTestUtil):
+
+  def test_select_data_target__keep(self):
+    csv_target = CSVDataTarget(source=self.csv_data_file, dtypes={'a': int, 'b': str})
+    target = undertest.SelectDataTarget(csv_target, ['a'], keep=True)
+    items = [item for item in target]
+    self.assertEqual(2, len(items))
+    self.assertEqual([{'a': 1}, {'a': 2}], items)
+
+
+  def test_select_data_target__discard(self):
+    csv_target = CSVDataTarget(source=self.csv_data_file, dtypes={'a': int, 'b': str})
+    target = undertest.SelectDataTarget(csv_target, ['a'], keep=False)
+    items = [item for item in target]
+    self.assertEqual(2, len(items))
+    self.assertEqual([{'b': 'x'}, {'b': 'y'}], items)
