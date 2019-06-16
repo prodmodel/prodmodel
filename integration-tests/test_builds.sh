a402889a510e68273363b5f4c42ce2c19e97ae1e
Gergely Svigruha <sgergo88@gmail.com>
2019-06-16 17:21:03 -0400

    Add to integration test.

--- integration-tests/test_builds.sh
+++ integration-tests/test_builds.sh
@@ -8,3 +8,5 @@ cd $BASEDIR
 python3.6 -m prodmodel example:test_transform
 python3.6 -m prodmodel example:evaluate
 python3.6 -m prodmodel example:model_in_s3
+
+python3.6 -m prodmodel example-s3:csv_data
