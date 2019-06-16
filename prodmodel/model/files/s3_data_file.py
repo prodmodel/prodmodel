451e136583c35e42778a0e6296a06e76dd530f61
Gergely Svigruha <sgergo88@gmail.com>
2019-06-16 17:19:37 -0400

    Nits.

--- prodmodel/model/files/s3_data_file.py
+++ prodmodel/model/files/s3_data_file.py
@@ -18,8 +18,8 @@ class S3DataFile(InputFile):
     super().__init__(file_name=s3_local_file_name(s3_path))
     self.cached_build_time = None
     self.s3_path = s3_path
-    self.bucket = s3_path[5:s3_path.index('/', 5)]
-    self.key = s3_path[s3_path.index('/', 5)  + 1:]
+    self.s3_bucket = s3_path[5:s3_path.index('/', 5)]
+    self.s3_key = s3_path[s3_path.index('/', 5)  + 1:]
     self.s3 = None
 
 
@@ -42,17 +42,17 @@ class S3DataFile(InputFile):
     else:
       local_last_modified = None
 
-    response = self._s3().get_object(Bucket=self.bucket, Key=self.key)
+    response = self._s3().get_object(Bucket=self.s3_bucket, Key=self.s3_key)
     s3_last_modified = response['LastModified'].isoformat()
     if local_last_modified != s3_last_modified:
       self.file_name.parent.mkdir(parents=True, exist_ok=True)
-      logging.debug(f'Downloading s3://{self.bucket}/{self.key} to {self.file_name}.')
+      logging.debug(f'Downloading s3://{self.s3_bucket}/{self.s3_key} to {self.file_name}.')
       with open(self.file_name, 'wb') as f:
         f.write(response['Body'].read())
       with open(metadata_file, 'w') as f:
         json.dump({'last_modified': s3_last_modified}, f)
     else:
-      logging.debug(f'Using cached version of s3://{self.bucket}/{self.key}: {self.file_name}.')
+      logging.debug(f'Using cached version of s3://{self.s3_bucket}/{self.s3_key}: {self.file_name}.')
 
     self.cached_hash_id = self.hash_id()
     return create_dest_file(args, self)
