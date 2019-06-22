17c4e0e463613ac9a9969633dd515c6794528c35
Gergely Svigruha <sgergo88@gmail.com>
2019-06-22 17:22:32 -0400

    Rename file.

--- integration-tests/test_cleaning.sh
+++ integration-tests/test_cleaning.sh
@@ -6,8 +6,8 @@ cd $BASEDIR
 
 # Make sure csv_data target output exists.
 python3.6 -m prodmodel example:csv_data
-FILES=$(ls example/.target/output/csv_data | wc -l)
-if [ "$FILES" -eq 0 ]
+CACHE_FILES=$(ls example/.target/output/csv_data | wc -l)
+if [ "$CACHE_FILES" -eq 0 ]
   then
     echo "No output files at example/.target/output/csv_data."
     exit 1
@@ -15,8 +15,8 @@ fi
 
 # Cutoff date is in the past, files should still be there.
 python3.6 -m prodmodel clean example:csv_data --cutoff_date=2019-01-01T00:00:00
-FILES=$(ls example/.target/output/csv_data | wc -l)
-if [ "$FILES" -eq 0 ]
+CACHE_FILES=$(ls example/.target/output/csv_data | wc -l)
+if [ "$CACHE_FILES" -eq 0 ]
   then
     echo "No output files at example/.target/output/csv_data."
     exit 1
@@ -24,8 +24,8 @@ fi
 
 # Default cutoff date is now, files should be deleted.
 python3.6 -m prodmodel clean example:csv_data
-FILES=$(ls example/.target/output/csv_data | wc -l)
-if [ "$FILES" -gt 0 ]
+CACHE_FILES=$(ls example/.target/output/csv_data | wc -l)
+if [ "$CACHE_FILES" -gt 0 ]
   then
     echo "Output files not deleted at example/.target/output/csv_data."
     exit 1
