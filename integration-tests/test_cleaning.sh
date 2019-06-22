1bf9b17e4c3524cccd5ff0918c2c9865b3e268b3
Gergely Svigruha <sgergo88@gmail.com>
2019-06-22 16:19:23 -0400

    Use python in test.

--- integration-tests/test_cleaning.sh
+++ integration-tests/test_cleaning.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname "$0")/..
 cd $BASEDIR
 
 # Make sure csv_data target output exists.
-python3.6 -m prodmodel example:csv_data
+python -m prodmodel example:csv_data
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -eq 0 ]
   then
@@ -14,7 +14,7 @@ if [ "$FILES" -eq 0 ]
 fi
 
 # Cutoff date is in the past, files should still be there.
-python3.6 -m prodmodel clean example:csv_data --cutoff_date=2019-01-01T00:00:00
+python -m prodmodel clean example:csv_data --cutoff_date=2019-01-01T00:00:00
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -eq 0 ]
   then
@@ -23,7 +23,7 @@ if [ "$FILES" -eq 0 ]
 fi
 
 # Default cutoff date is now, files should be deleted.
-python3.6 -m prodmodel clean example:csv_data
+python -m prodmodel clean example:csv_data
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -gt 0 ]
   then
