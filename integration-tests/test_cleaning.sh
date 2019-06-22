528515bfb11c046ec6c9c57e0aa6225453182ff3
Gergely Svigruha <sgergo88@gmail.com>
2019-06-22 16:11:17 -0400

    Nits.

--- integration-tests/test_cleaning.sh
+++ integration-tests/test_cleaning.sh
@@ -5,6 +5,7 @@ BASEDIR=$(dirname "$0")/..
 source $BASEDIR/.env
 cd $BASEDIR
 
+# Make sure csv_data target output exists.
 python3.6 -m prodmodel example:csv_data
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -eq 0 ]
@@ -13,6 +14,7 @@ if [ "$FILES" -eq 0 ]
     exit 1
 fi
 
+# Cutoff date is in the past, files should still be there.
 python3.6 -m prodmodel clean example:csv_data --cutoff_date=2019-01-01T00:00:00
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -eq 0 ]
@@ -21,6 +23,7 @@ if [ "$FILES" -eq 0 ]
     exit 1
 fi
 
+# Default cutoff date is now, files should be deleted.
 python3.6 -m prodmodel clean example:csv_data
 FILES=$(ls example/.target/output/csv_data | wc -l)
 if [ "$FILES" -gt 0 ]
@@ -28,4 +31,3 @@ if [ "$FILES" -gt 0 ]
     echo "Output files not deleted at example/.target/output/csv_data."
     exit 1
 fi
-
