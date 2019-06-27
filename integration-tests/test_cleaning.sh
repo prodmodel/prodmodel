09f96aa9bac7688971d9f982f4b5d2acd7e6e333
Gergely Svigruha <gsvigruha@users.noreply.github.com>
2019-06-27 19:34:05 -0400

    Move tests. (#95)

--- integration-tests/test_cleaning.sh
+++ integration-tests/test_cleaning.sh
@@ -5,28 +5,28 @@ BASEDIR=$(dirname "$0")/..
 cd $BASEDIR
 
 # Make sure csv_data target output exists.
-python3.6 -m prodmodel example-test:csv_data
-CACHE_FILES=$(ls example-test/.target/output/csv_data | wc -l)
+python3.6 -m prodmodel integration-tests/example:csv_data
+CACHE_FILES=$(ls integration-tests/example/.target/output/csv_data | wc -l)
 if [ "$CACHE_FILES" -eq 0 ]
   then
-    echo "No output files at example-test/.target/output/csv_data."
+    echo "No output files at integration-tests/example/.target/output/csv_data."
     exit 1
 fi
 
 # Cutoff date is in the past, files should still be there.
-python3.6 -m prodmodel clean example-test:csv_data --cutoff_date=2019-01-01T00:00:00
-CACHE_FILES=$(ls example-test/.target/output/csv_data | wc -l)
+python3.6 -m prodmodel clean integration-tests/example:csv_data --cutoff_date=2019-01-01T00:00:00
+CACHE_FILES=$(ls integration-tests/example/.target/output/csv_data | wc -l)
 if [ "$CACHE_FILES" -eq 0 ]
   then
-    echo "No output files at example-test/.target/output/csv_data."
+    echo "No output files at integration-tests/example/.target/output/csv_data."
     exit 1
 fi
 
 # Default cutoff date is now, files should be deleted.
-python3.6 -m prodmodel clean example-test:csv_data
-CACHE_FILES=$(ls example-test/.target/output/csv_data | wc -l)
+python3.6 -m prodmodel clean integration-tests/example:csv_data
+CACHE_FILES=$(ls integration-tests/example/.target/output/csv_data | wc -l)
 if [ "$CACHE_FILES" -gt 0 ]
   then
-    echo "Output files not deleted at example-test/.target/output/csv_data."
+    echo "Output files not deleted at integration-tests/example/.target/output/csv_data."
     exit 1
 fi
