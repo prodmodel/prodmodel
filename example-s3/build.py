98c58aa6ddb46b7ad7da24a304fee7bd4d8798da
Gergely Svigruha <sgergo88@gmail.com>
2019-06-29 12:05:42 -0400

    Use smaller s3 file.

--- example-s3/build.py
+++ example-s3/build.py
@@ -1,31 +1,16 @@
 from prodmodel.rules import aws_rules, rules
 
 csv_data = rules.data_stream(
-  file='s3://prodmodel-test/sources/data.csv',
+  file='s3://prodmodel-test/sources/data-small.csv',
   data_type='csv',
   dtypes = {
     'age': int,
-    'job': str,
-    'marital': str,
-    'education': str,
-    'default': str,
-    'balance': int,
-    'housing': str,
-    'loan': str,
-    'contact':str,
-    'day':int,
-    'month':str,
-    'duration':int,
-    'campaign':int,
-    'pdays':int,
-    'previous':int,
-    'poutcome':str,
-    'y':str
+    'job': str
   }
 )
 
 data_in_s3 = aws_rules.copy_to_s3(
   data=csv_data,
   s3_bucket='prodmodel-test',
-  s3_key='data/data.csv'
+  s3_key='dest/data-small.csv'
 )
