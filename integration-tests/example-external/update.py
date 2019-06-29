7395c6f0e08f4afc725b2ba4670544a16fd70271
Gergely Svigruha <sgergo88@gmail.com>
2019-06-29 18:52:19 -0400

    Fix test.

--- integration-tests/example-external/update.py
+++ integration-tests/example-external/update.py
@@ -3,10 +3,9 @@ import sqlite3
 from pathlib import Path
 
 
-def __main__():
-  path = str(Path(os.path.dirname(__file__)) / 'remote.db')
-  conn = sqlite3.connect(path)
-  try:
-    cursor.execute('update education_lookup set score=300 where education="secondary"')
-  finally:
-    conn.close()
+path = str(Path(os.path.dirname(__file__)) / 'remote.db')
+conn = sqlite3.connect(path)
+cursor = conn.cursor()
+cursor.execute('update education_lookup set score=300 where education="secondary"')
+conn.commit()
+conn.close()
