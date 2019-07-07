c4c153cde06ee55cdc85615f085d07f5ad143a34
Gergely Svigruha <sgergo88@gmail.com>
2019-07-07 13:17:06 -0400

    Add integration tests.

--- integration-tests/example/build.py
+++ integration-tests/example/build.py
@@ -23,3 +23,28 @@ csv_data = rules.data_stream(
     'y':str
   }
 )
+
+csv_data_to_json = rules.data_stream(
+  file='data.csv',
+  data_type='csv',
+  dtypes = {
+    'age': int,
+    'job': str,
+    'marital': str,
+    'education': str,
+    'default': str,
+    'balance': int,
+    'housing': str,
+    'loan': str,
+    'contact':str,
+    'day':int,
+    'month':str,
+    'duration':int,
+    'campaign':int,
+    'pdays':int,
+    'previous':int,
+    'poutcome':str,
+    'y':str
+  },
+  output_format='json'
+)
