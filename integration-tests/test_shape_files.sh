3369bb9f29000d035778cce0b958c658badb7d61
Gergely Svigruha <sgergo88@gmail.com>
2019-07-14 22:56:51 -0400

    Regen doc.

--- integration-tests/test_shape_files.sh
+++ integration-tests/test_shape_files.sh
@@ -9,6 +9,6 @@ python3.5 -m prodmodel build integration-tests/example:shape_file --output_forma
 CACHE_FILES=$(ls integration-tests/example/.target/output/shape_file/*/output_1.pickle | wc -l)
 if [ "$CACHE_FILES" -eq 0 ]
   then
-    echo "No json output files at integration-tests/example/.target/output/shape_file."
+    echo "No output files at integration-tests/example/.target/output/shape_file."
     exit 1
 fi
